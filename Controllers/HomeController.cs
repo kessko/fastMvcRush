0c0b5caa6c845b3c5d6beed82369a874d702a197
kessko <e.krovkin@gmail.com>
2015-04-07 21:39:15 +0300

    Added project file

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using fastMvcRush.Models;
+
+namespace fastMvcRush.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.ComeText = "Alloha Man";
+            return View();
+        }
+        [HttpGet]
+        public ActionResult RNSV()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RNSV(GuestRequest guestRequest)
+        {
+            //TODO send email to guest
+            if (ModelState.IsValid)
+            {
+                return View("GuestResponse", guestRequest);
+            }
+            else
+            {
+                return View();
+            }
+        
+        }
+
+    }
+}
---
+++ Models/GuestRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace fastMvcRush.Models
+{
+    public class GuestRequest
+    {
+        [Required(ErrorMessage="InputName")]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool? Come { get; set; }
+    }
+}
